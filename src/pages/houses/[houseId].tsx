79ada70b7f0c1bb8c47106f91861e99a38a70bf2
Astrid-Froitier <asfroitier@gmail.com>
2023-01-30 12:52:40 +0100

    displayed house information

--- src/pages/houses/[houseId].tsx
+++ src/pages/houses/[houseId].tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 
-function HouseDetailPage() {
-  return <h1>HouseDetailPage</h1>;
+import { IHouse } from '../../../interfaces/IHouse';
+import { Base_Houses } from '../../utils/constants';
+
+type IProps = { house: IHouse };
+
+function HouseDetailPage({ house }: IProps): JSX.Element {
+  return (
+    <Fragment>
+      <div className="h-screen text-white bg-black">
+        <h1 className="flex items-center justify-center h-40 text-4xl italic">{house.name}</h1>
+        <div className="flex flex-col gap-2 pl-12">
+          <p>{house.currentLord}</p>
+          <p>{house.coatOfArms}</p>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+  const houseId = JSON.stringify(params?.houseId);
+  const id = JSON.parse(houseId);
+
+  const oneHouse = await axios.get(`${Base_Houses}/${id}`);
+  const house = oneHouse.data;
+
+  return {
+    props: {
+      id: id,
+      house: house,
+    },
+  };
+};
 
 export default HouseDetailPage;
